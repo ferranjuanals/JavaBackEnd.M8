c71f83d0e52e754930a8862c9fc800c44372a7e6
Ferran Juanals <ferranjuanals@gmail.com>
2020-12-21 10:49:38 +0100

    Exercici JavaBackEnd.M8 - Fase 2

--- src/com/video/project/Coet.java
+++ src/com/video/project/Coet.java
@@ -1,16 +1,23 @@
 package com.video.project;
 
+import java.util.Arrays;
+
 public class Coet {
 	
 	private String codi;
 	private int propulsors;
+	private int[] potencies;
 	
-	public Coet(String codi, int propulsors) {
+	public Coet(String codi, int propulsors, int[] potencies) throws Exception{
+		if(codi.length() != 8) throw new Exception("El codi del coet ha de ser de 8 xifres");
+		if(propulsors != potencies.length) throw new Exception("S'ha d'introduïr una potència màxima per a cada propulsor");
+		
 		this.codi = codi;
 		this.propulsors = propulsors;
+		this.potencies = potencies;
 	}
 
 	public void getInfo() {
-		System.out.println("El codi del coet és: " + codi + ". Té " + propulsors + " propulsors");
+		System.out.println("El codi del coet és: " + codi + ". Té " + propulsors + " propulsors amb les següents potències màximes: " + Arrays.toString(potencies));
 	}
 }
--- src/com/video/project/Main.java
+++ src/com/video/project/Main.java
@@ -2,11 +2,13 @@ package com.video.project;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
-		Coet coet1 = new Coet("x", 3);
-		Coet coet2 = new Coet("LDSFJA32", 6);
+		// Es creen dos coets
+		Coet coet1 = new Coet("32WESSDS", 3, new int[] {10, 30, 80});
+		Coet coet2 = new Coet("LDSFJA32", 6, new int[] {30, 40, 50, 50, 30, 10});
 		
+		// Es mostra la informació
 		coet1.getInfo();
 		coet2.getInfo();
 
